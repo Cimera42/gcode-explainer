0b935d58c80e5416a07e91f9e3f71bac34e2de3b
Tim Porritt <tim.m.porritt@gmail.com>
2018-12-09 22:21:14 +1100

    Fixed header link

--- src/Parts/Header/Header.jsx
+++ src/Parts/Header/Header.jsx
@@ -5,7 +5,12 @@ import githubLogo from './GitHub-Mark-Light-120px-plus.png';
 const Header = (props) => {
 	return (
 		<header styleName="container">
-			<a href="/" styleName="heading">G-code Explainer</a>
+			<a
+				href="https://cimera42.github.io/gcode-explainer/"
+				styleName="heading"
+			>
+				G-code Explainer
+			</a>
 			<div styleName="links">
 				<a
 					href="https://github.com/Cimera42/gcode-explainer"
