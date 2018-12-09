95a6d70b926a729ac7f4c0085ea19d3a40a55667
Tim Porritt <tim.m.porritt@gmail.com>
2018-12-09 21:52:42 +1100

    Github link

--- src/Parts/Header/Header.jsx
+++ src/Parts/Header/Header.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import "./Header.scss";
+import githubLogo from './GitHub-Mark-Light-120px-plus.png';
 
 const Header = (props) => {
 	return (
 		<header styleName="container">
 			<Link to="/" styleName="heading">G-code Explainer</Link>
 			<div styleName="links">
-				<Link to="/">G-code Explainer</Link>
+				<a
+					href="https://github.com/Cimera42/gcode-explainer"
+					target="_blank"
+					rel="noopener noreferrer"
+					styleName="vertical-center"
+				>
+					<img
+						src={githubLogo} alt="github logo"
+						styleName="icon-logo"
+					/>
+					G-code Explainer
+				</a>
 			</div>
 		</header>
 	);
