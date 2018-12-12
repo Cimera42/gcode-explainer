fa405877600c6bd406a61980f2b7128551464bbf
Tim Porritt <tim.m.porritt@gmail.com>
2018-12-13 08:27:47 +1100

    Delete all and move docs into main directory

--- .github/gitstuff/entrypoint.sh
+++ .github/gitstuff/entrypoint.sh
@@ -4,11 +4,14 @@ git config user.name "GitHub Actions"
 git config user.email "cimera2@gmail.com"
 
 git checkout -b gh-pages
-git add --all
 git rm -r ./.github
+rm -rf node_modules
+find . -maxdepth 1 -not -name "docs" -not -name ".git" -not -name "." -not -name ".gitignore" | xargs git rm -r
+mv ./docs/* .
+git add --all
 git commit -m "Automated build"
 
 git remote rm origin
 git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/Cimera42/gcode-explainer.git
 
-git push origin gh-pages
+git push origin gh-pages --force
