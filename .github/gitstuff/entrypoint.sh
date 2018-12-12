8c2cb348219b04d3685b563bb4828bb178ccd1fc
Tim Porritt <tim.m.porritt@gmail.com>
2018-12-13 08:09:01 +1100

    Action documentation, gh-pages branch

--- .github/gitstuff/entrypoint.sh
+++ .github/gitstuff/entrypoint.sh
@@ -3,12 +3,12 @@
 git config user.name "GitHub Actions"
 git config user.email "cimera2@gmail.com"
 
-git checkout -b deploy
+git checkout -b gh-pages
 git add --all
 git rm -r ./.github
-git commit -m "Test"
+git commit -m "Automated build"
 
 git remote rm origin
 git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/Cimera42/gcode-explainer.git
 
-git push origin deploy
+git push origin gh-pages
